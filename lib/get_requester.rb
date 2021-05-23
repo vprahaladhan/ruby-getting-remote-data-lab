467de89b5fcbf93dbc1b5ab882b8b5b4413392c8
prahal <vprahaladhan@gmail.com>
2021-05-23 11:32:35 +1000

    commit tree 759ca9388534d034bbb904c7d675892b0b996acd

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+# require 'awesome_print'
+
+class GetRequester
+  attr_accessor :URL
+
+  def initialize(url)
+    @URL = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(self.get_response_body)
+  end
+end
